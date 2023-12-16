080373fd792bb7726f31642c0484a73019a7ac83
RazoelZ <danydarmawannn@gmail.com>
2023-12-17 04:56:45 +0700

    fix : migrate

--- index.js
+++ index.js
@@ -4,21 +4,10 @@ import router from './routes/index.js';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-// import Users from './models/UserModels.js';
-// import Nutrisions from './models/NutrisionsModel.js';
-// import Meals from './models/MealsModel.js';
-// import FactHealths from './models/FactHealthsModel.js';
-
+import Migrate from './config/Migrate.js';
 
 dotenv.config();
-
 const app = express();
-
-//start server
-app.listen(5000, () => {
-    console.log('Server on port', 5000);
-}
-);
 
 app.use(cors({
     origin: 'http://localhost:5000',
@@ -28,17 +17,20 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(router);
 
-//test db
 try {
     db.authenticate();
     console.log('Database connected');
 
-    // line below is for create table
-    // await Users.sync();
-    // await Nutrisions.sync();
-    // await Meals.sync();
-    // await FactHealths.sync();
+    // make migrations
+    // Migrate();
+
 }
 catch (error) {
     console.error('Unable to connect to the database:', error);
 }
+
+
+app.listen(5000, () => {
+    console.log('Server on port', 5000);
+}
+);
