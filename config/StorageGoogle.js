f95751f66bdfbf3894b78d63a04e8376386e323d
RazoelZ <danydarmawannn@gmail.com>
2023-12-17 05:35:20 +0700

    test : deploy first time

--- config/StorageGoogle.js
+++
@@ -1,8 +0,0 @@
-import { Storage } from '@google-cloud/storage';
-
-const storage = new Storage({
-    projectId: 'capstone-trackmeals-405419',
-    keyFilename: 'key.json',
-});
-
-export default storage;
